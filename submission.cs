91f98580bf7b7ed096c416552cf6786d9bc4b1d6
HainesRoy389 <140041112+HainesRoy389@users.noreply.github.com>
2025-04-08 04:47:28 -0700

    Update submission.cs
    Used provided template

--- submission.cs
+++ submission.cs
@@ -1,41 +1,57 @@
 using System;
 using System.Xml.Schema;
 using System.Xml;
+using Newtonsoft.Json;
+using System.IO;
+
+
+
+/**
+ * This template file is created for ASU CSE445 Distributed SW Dev Assignment 4.
+ * Please do not modify or delete any existing class/variable/method names. However, you can add more variables and functions.
+ * Uploading this file directly will not pass the autograder's compilation check, resulting in a grade of 0.
+ * **/
+
+
 namespace ConsoleApp1
 {
- public class Program
- {
-    // These URLs will be read by the autograder, please keep the variable name un-changed and link to the correct xml/xsd
-    //files.
-    public static string xmlURL = "https://hainesroy389.github.io/CSE445Project4/Hotels.xml"; //Q1.2
-    public static string xmlErrorURL = "https://hainesroy389.github.io/CSE445Project4/HotelsErrors.xml"; //Q1.3
-    public static string xsdURL = "https://hainesroy389.github.io/CSE445Project4/Hotels.xsd"; //Q1.1
-    
-    public static void Main(string[] args)
+    public class Program
     {
-        // Q3: You can pick two of three
-        string result = Verification(xmlURL, xsdURL);
-        Console.WriteLine(result);
-        result = Verification(xmlErrorURL, xsdURL);
-        Console.WriteLine(result);
-        result = Xml2Json("Hotels.xml");
-        Console.WriteLine(result);
+        public static string xmlURL = "https://hainesroy389.github.io/CSE445Project4/Hotels.xml";
+        public static string xmlErrorURL = "https://hainesroy389.github.io/CSE445Project4/HotelsErrors.xml";
+        public static string xsdURL = "https://hainesroy389.github.io/CSE445Project4/Hotels.xsd";
+
+        public static void Main(string[] args)
+        {
+            string result = Verification(xmlURL, xsdURL);
+            Console.WriteLine(result);
+
+
+            result = Verification(xmlErrorURL, xsdURL);
+            Console.WriteLine(result);
+
+
+            result = Xml2Json(xmlURL);
+            Console.WriteLine(result);
+        }
+
+        // Q2.1
+        public static string Verification(string xmlUrl, string xsdUrl)
+        {
+
+
+            //return "No Error" if XML is valid. Otherwise, return the desired exception message.
+            return "";
+        }
+
+        public static string Xml2Json(string xmlUrl)
+        {
+            
+
+            // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
+            //return jsonText;
+            return "";
+
+        }
     }
-    
-    // Q2.1
-    public static string Verification(string xmlUrl, string xsdUrl)
-    {
-        //return "No Error" if XML is valid. Otherwise, return the desired exception message.
-        return "";
-    }
-    
-    // Q2.2
-    public static string Xml2Json(string xmlUrl)
-    {
-        // The returned jsonText needs to be de-serializable by Newtonsoft.Json package.
-        //(JsonConvert.DeserializeXmlNode(jsonText))
-        //return jsonText;
-        return "";
-    }
- }
 }
